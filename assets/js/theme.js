056d7b36cdc8c305f848ad5fd9a05c9e72248b43
diego-tavares <cdiegostavares@gmail.com>
2023-04-15 11:47:59 -0300

    first commit

---
+++ assets/js/theme.js
@@ -0,0 +1,33 @@
+function setTheme(theme) {
+    if (theme === 'blue') {
+        document.documentElement.style.setProperty('--main-color', '#1c8adb');
+        document.documentElement.style.setProperty('--text-color', '#ffffff');
+        document.documentElement.style.setProperty('--header-bg-color', '#235a8c');
+        document.documentElement.style.setProperty('--header-text-color', '#ffffff');
+        document.documentElement.style.setProperty('--even-row-bg-color', '#f0f8ff');
+        document.documentElement.style.setProperty('--odd-row-bg-color', '#ffffff');
+        document.documentElement.style.setProperty('--border-color', '#d3d3d3');
+        document.documentElement.style.setProperty('--editable-cell-bg-color', '#f5deb3');
+        document.documentElement.style.setProperty('--editable-cell-text-color', '#000000');
+        } else if (theme === 'green') {
+        document.documentElement.style.setProperty('--main-color', '#2ecc71');
+        document.documentElement.style.setProperty('--text-color', '#ffffff');
+        document.documentElement.style.setProperty('--header-bg-color', '#27ae60');
+        document.documentElement.style.setProperty('--header-text-color', '#ffffff');
+        document.documentElement.style.setProperty('--even-row-bg-color', '#f0f8ff');
+        document.documentElement.style.setProperty('--odd-row-bg-color', '#ffffff');
+        document.documentElement.style.setProperty('--border-color', '#d3d3d3');
+        document.documentElement.style.setProperty('--editable-cell-bg-color', '#f5deb3');
+        document.documentElement.style.setProperty('--editable-cell-text-color', '#000000');
+        } else if (theme === 'red') {
+        document.documentElement.style.setProperty('--main-color', '#e74c3c');
+        document.documentElement.style.setProperty('--text-color', '#ffffff');
+        document.documentElement.style.setProperty('--header-bg-color', '#c0392b');
+        document.documentElement.style.setProperty('--header-text-color', '#ffffff');
+        document.documentElement.style.setProperty('--even-row-bg-color', '#f0f8ff');
+        document.documentElement.style.setProperty('--odd-row-bg-color', '#ffffff');
+        document.documentElement.style.setProperty('--border-color', '#d3d3d3');
+        document.documentElement.style.setProperty('--editable-cell-bg-color', '#f5deb3');
+        document.documentElement.style.setProperty('--editable-cell-text-color', '#000000');
+        }
+        }
